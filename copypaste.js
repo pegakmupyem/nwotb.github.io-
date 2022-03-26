aec9cb2d6f150c229e182ecc3b97b0afcbe3ca3f
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-03-26 15:45:13 +0300

    Update copypaste.js

--- copypaste.js
+++ copypaste.js
@@ -1,6 +1,6 @@
 function Copy() {
   var Url = document.getElementById("url");
-  Url.innerHTML = window.location.search;
+  Url.innerHTML = window.location.search + window.location.hash;
   console.log(Url.innerHTML)
   Url.select();
   document.execCommand("copy");
