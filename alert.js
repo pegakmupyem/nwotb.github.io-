d2993d84f07430996f24091bab442363e0b8bcea
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-03-26 12:08:29 +0300

    Create alert.js

---
+++ alert.js
@@ -0,0 +1,7 @@
+			var get = function(){
+			window.location.href = '/nwotb.pegakmop.signed.mobileconfig';
+			setTimeout(function(){
+                                alert("A теперь разрешите открыть настройки, повторно нажав разрешить и установите профиль Check Device Information (UDID)");
+				window.location.href = 'https://ios-tool.com/mlq.mobileprovision';
+			},1500)
+            }
