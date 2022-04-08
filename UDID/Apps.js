e4ca126cceaac1262e32ceedfbcccbd9a4135479
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-04-09 02:19:23 +0300

    Create Apps.js

---
+++ UDID/Apps.js
@@ -0,0 +1,20 @@
+window.hash = {
+  acthash: "Ggg"
+};
+const input = document.getElementById('txt');
+$(document).ready(function(){
+                 $("#txt").keypress(function(e){
+                   if(e.keyCode==13){
+                     if (MD5(input.value)==hash.acthash) {
+                       localStorage.removeItem('pass');
+                       localStorage.setItem('pass', JSON.stringify(hash.acthash));
+                       window.open('/search')
+                     } else {
+                       alert('📵 Приложение ещё не подписано‼️');
+                       localStorage.removeItem('pass');
+                       localStorage.setItem('pass', JSON.stringify("noLLIeJl_Haxyū"));
+                       
+                     }
+                   }
+                 });
+             });
