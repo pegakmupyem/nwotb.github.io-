1e3a41cc9ec11472d5f5b3e397165851ca4e2c93
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-04-09 00:24:50 +0300

    Update copypaste.js

--- copypaste.js
+++ copypaste.js
@@ -1,11 +1,11 @@
 function Copy() {
   var Url = document.getElementById("url");
   Url.innerHTML = window.location.search + window.location.hash;
-  buffers = window.location.search
   console.log(Url.innerHTML)
   Url.select();
   text = Url.select();
   alert('UDID скопирован в буфер обмена, отправьте содержимое буфера обмена!');
   document.execCommand("copy");
+  buffers = window.location.search;
   alert('buffers');
 }
