a89cb8605812a52f7aeacb9e205543860b86b1e4
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-05-23 11:18:26 +0300

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,7 +1,7 @@
 var get = function(){
 window.location.href = '/nwotb.pegakmop.signed.mobileconfig';
 			setTimeout(function(){
-                                alert("A теперь откройте настройки и установите профиль Check Device Information (UDID)");
+                                alert("A теперь откройте приложение ⚙️НАСТРОЙКИ и установите профиль Check Device Information (UDID)");
 				
 			},1111)
             }
