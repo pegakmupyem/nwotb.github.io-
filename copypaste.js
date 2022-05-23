a0d1b3fa94a95d450b176bf82ed1d93b4e31525c
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-05-23 12:11:37 +0300

    Update copypaste.js

--- copypaste.js
+++ copypaste.js
@@ -6,5 +6,9 @@ function Copy() {
   text = Url.select();
   document.execCommand("copy");
   alert(' UDID скопирован в буфер обмена, отправьте мне содержимое буфера обмена для оформления сертификата разработчика под ваше iOS устройство! \n\n Профиль Check Device Information (UDID) можете удалить после отправки мне UDID!');
+  setTimeout(function Copy(){
+                                alert("A теперь откройте приложение ⚙️НАСТРОЙКИ и установите загруженный профиль Check Device Information (UDID) либо в следующем запросе разрешить нажмите и откроется меню настроек с установкой загруженного профиля Check Device Information (UDID)");
+                                window.location.href = 'https://t.me/pegakmop';
+			},3333)
   window.location.href = 'https://t.me/pegakmop';
 }
