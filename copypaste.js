485a82f4683057acfc2f6e66baefba21e125196a
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-05-23 13:15:28 +0300

    Update copypaste.js

--- copypaste.js
+++ copypaste.js
@@ -6,5 +6,5 @@ function Copy() {
   text = Url.select();
   document.execCommand("copy");
   setTimeout(() => alert(' 1 UDID скопирован в буфер обмена, отправьте мне содержимое буфера обмена для оформления сертификата разработчика под ваше iOS устройство! \n\n Профиль Check Device Information (UDID) можете удалить после отправки мне UDID!'), 3333);
-  setTimeout(function(){ window.location="https://t.me/pegakmop"; },5000);
+  setTimeout(function(){ window.location="https://t.me/pegakmop"; },6666);
 }
