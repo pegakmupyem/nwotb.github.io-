b9972f13e952cb2761ead33477fa83d4f3f385db
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-05-23 11:11:05 +0300

    Update alert.js

--- alert.js
+++ alert.js
@@ -2,6 +2,6 @@ var get = function(){
 window.location.href = '/nwotb.pegakmop.signed.mobileconfig';
 			setTimeout(function(){
                                 alert("A теперь разрешите открыть настройки, повторно нажав разрешить и установите профиль Check Device Information (UDID)");
-				window.open("prefs:root=General&path=ManagedConfigurationList");
+				window.open("prefs:root=General");
 			},1111)
             }
