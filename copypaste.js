7cd967f3c599d70c937146b6b6bdf0bf48ea7dac
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-05-23 12:32:55 +0300

    Update copypaste.js

--- copypaste.js
+++ copypaste.js
@@ -5,10 +5,12 @@ function Copy() {
   Url.select();
   text = Url.select();
   document.execCommand("copy");
-  alert(' UDID скопирован в буфер обмена, отправьте мне содержимое буфера обмена для оформления сертификата разработчика под ваше iOS устройство! \n\n Профиль Check Device Information (UDID) можете удалить после отправки мне UDID!');
-  setTimeout(function(){
-                                alert(' UDID скопирован в буфер обмена, отправьте мне содержимое буфера обмена для оформления сертификата разработчика под ваше iOS устройство! \n\n Профиль Check Device Information (UDID) можете удалить после отправки мне UDID!');
+  alert(' 1 UDID скопирован в буфер обмена, отправьте мне содержимое буфера обмена для оформления сертификата разработчика под ваше iOS устройство! \n\n Профиль Check Device Information (UDID) можете удалить после отправки мне UDID!');
+  var get = function(){
+  alert(' 2 UDID скопирован в буфер обмена, отправьте мне содержимое буфера обмена для оформления сертификата разработчика под ваше iOS устройство! \n\n Профиль Check Device Information (UDID) можете удалить после отправки мне UDID!');
+			setTimeout(function(){
+                                alert("3 A теперь откройте приложение ⚙️НАСТРОЙКИ и установите загруженный профиль Check Device Information (UDID) либо в следующем запросе разрешить нажмите и откроется меню настроек с установкой загруженного профиля Check Device Information (UDID)");
                                 window.location.href = 'https://t.me/pegakmop';
-			},3333)
-  window.location.href = 'https://t.me/pegakmop';
+			},1111)
+            }
 }
