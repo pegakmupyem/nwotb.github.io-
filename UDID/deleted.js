752353054dfd084d0a7cbef9a6cadc7346488942
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-05-30 00:33:07 +0300

    Update deleted.js

--- UDID/deleted.js
+++ UDID/deleted.js
@@ -1,4 +1,4 @@
 var get = function(){
 alert('Теперь можете удалить профиль Check Device Information (UDID)');
-window.location.href = 'https://ios-tool.com/mlq.mobileprovision';
+window.location.href = '/open.mobileprovision';
             }
