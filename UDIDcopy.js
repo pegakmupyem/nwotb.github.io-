35fe8f1383db081a99507f1f8e1b0c60c48fa3bf
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-05-30 08:30:52 +0300

    Update UDIDcopy.js

--- UDIDcopy.js
+++ UDIDcopy.js
@@ -1,6 +1,6 @@
 function Copy() {
   var Url = document.getElementById("url");
-  Url.innerHTML = window.location.search + window.location.hash;
+  Url.innerHTML = window.location.search + window.location.hash.slice(1);;
   console.log(Url.innerHTML)
   Url.select();
   text = Url.select();
