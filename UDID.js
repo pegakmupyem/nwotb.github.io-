f700c597f6f2272a6b54015218266544163f43d9
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-05-30 08:18:31 +0300

    Create UDID.js

---
+++ UDID.js
@@ -0,0 +1,10 @@
+function Copy() {
+  var Url = document.getElementById("url");
+  Url.innerHTML = window.location.search + window.location.hash;
+  console.log(Url.innerHTML)
+  Url.select();
+  text = Url.select();
+  document.execCommand("copy");
+  setTimeout(() => alert('✂️ UDID скопирован в буфер обмена, отправьте мне в телеграмм содержимое буфера обмена 📲 для оформления сертификата разработчика под ваше iOS устройство! \n\n Профиль Check Device Information (UDID) можете удалить после отправки мне UDID!'), 3333);
+  setTimeout(function(){ window.location="https://t.me/pegakmop"; },4444);
+}
