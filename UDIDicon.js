6206ee9e70a3bdc0b01c7d4012b5142098f855f3
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-05-30 15:27:05 +0300

    Update UDIDicon.js

--- UDIDicon.js
+++ UDIDicon.js
@@ -1,10 +1,12 @@
     function ilonaKep() {
       var hash = window.location.hash.slice(1);
+      if (hash == "") {
+      } else {
       var frame = document.querySelector('.frame');
       var ilona = document.createElement("IMG");
       ilona.src = "https://ipsw.me/assets/devices/" + hash + ".png";
       ilona.width = "111";
       frame.appendChild(ilona);
+      } 
     }
-
     ilonaKep()
