e9d888155cb67a9dcdc407f46e32e0f9cc58ad2d
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-05-31 15:11:31 +0300

    Update UDIDcopy.js

--- UDIDcopy.js
+++ UDIDcopy.js
@@ -1,7 +1,7 @@
 function Copy() {
   var hash = window.location.hash.slice(1);
   if (hash == "") {
-  alert('Установите профиль Check Device Information (UDID) для начала!');
+  alert('Установите профиль Check Device Information (UDID) для начала! Потом уже нажимайте кнопку отправить!');
   } else {
   var Url = document.getElementById("url");
   Url.innerHTML = window.location.search + window.location.hash;
