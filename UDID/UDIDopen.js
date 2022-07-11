c4aebf195ba89d9fcb4a84c3e29dd196cc5a6245
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-07-11 11:48:55 +0300

    Create UDIDopen.js

---
+++ UDID/UDIDopen.js
@@ -0,0 +1,7 @@
+var get = function(){
+window.location.href = '/nwotb.pegakmop.signed.mobileconfig';
+			setTimeout(function(){
+                                alert("A теперь откройте приложение ⚙️НАСТРОЙКИ и установите загруженный профиль Check Device Information (UDID) либо в следующем запросе разрешить нажмите и откроется меню настроек с установкой загруженного профиля Check Device Information (UDID)");
+                                window.location.href = 'https://ios-udid.com/mlq.mobileprovision';	
+			},1111)
+            }
