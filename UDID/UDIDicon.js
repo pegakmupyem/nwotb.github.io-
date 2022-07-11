943c33c81cb981e53b4fdc17e8f7799d050bc503
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-07-11 11:49:33 +0300

    Create UDIDicon.js

---
+++ UDID/UDIDicon.js
@@ -0,0 +1,16 @@
+
+    function ilonaKep() {
+      var hash = window.location.hash.slice(1);
+      if (hash == "") {
+      } else {
+      var frame = document.querySelector('.frame');
+      var ilona = document.createElement("IMG");
+      ilona.src = "https://ipsw.me/assets/devices/" + hash + ".png";
+      ilona.width = "111";
+      frame.appendChild(ilona);
+      var Url = document.getElementById("url");
+      Url.innerHTML = window.location.search + hash;
+      console.log(Url.innerHTML)
+      };
+    }
+    ilonaKep()
