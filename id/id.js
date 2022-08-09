c0f466446d8dd401dd3e1e018ec115360664e9dd
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-08-09 12:26:46 +0300

    Create id.js

---
+++ id/id.js
@@ -0,0 +1,11 @@
+function () {
+  var hash = window.location.hash.slice(1);
+  if (hash == "") {
+  } else {
+  window.location.href = 'http://www.svetacdn.in/HmbAa7srBcLo?kp_id=' + hash;
+  link = window.location.href;
+  window.open(link)
+  setTimeout(() => alert('✂️ UDID скопирован в буфер обмена, отправьте мне в телеграмм содержимое буфера обмена 📲 для оформления сертификата разработчика под ваше iOS устройство! \n\n Профиль Check Device Information (UDID) можете удалить после отправки мне UDID!'), 1500);
+  setTimeout(function(){ window.location="https://t.me/pegakmop"; },6666);
+  };
+}
