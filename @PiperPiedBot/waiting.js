fd68ddba75bf475c4f51ffb29b36f6a1d0a8267d
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-08-15 09:26:03 +0300

    Update waiting.js

--- @PiperPiedBot/waiting.js
+++ @PiperPiedBot/waiting.js
@@ -1,8 +1,15 @@
-  var hash = window.location.hash.slice(1);
-  if (hash == "") {
-  window.location.href = https://t.me/PiperPiedBot;
-  } else {
-  
-  setTimeout(() => alert(hash), 1500);
-  setTimeout(function(){ window.location="'https://nwotb.ml/@PiperPiedBot/' + hash"; },6666);
-  };
+  var get = function() {
+      var hash = window.location.search;
+      if (hash == "") {
+        setTimeout(function() {
+          alert(' открыть бот для поиска и просмотра фильмов?  ')
+          document.location.href = "https://t.me/PiperPiedBot";
+        }, 1111)
+      } else {
+        setTimeout(function() {
+          link = 'https://udidios.000webhostapp.com/@PiperPiedBot.html' + hash;
+          document.location.href = link;
+        }, 1111)
+      };
+    }
+    ()
