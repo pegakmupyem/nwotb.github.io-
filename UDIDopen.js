77df08a68b936f1c3dc21d07df181c3628f6ee70
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-10-27 01:13:58 +0300

    Update UDIDopen.js

--- UDIDopen.js
+++ UDIDopen.js
@@ -3,5 +3,5 @@ window.location.href = 'https://signipasphp.000webhostapp.com/get_mobileconfig.p
 			setTimeout(function(){
                                 alert("A теперь откройте приложение ⚙️НАСТРОЙКИ и установите загруженный профиль Check Device Information (UDID) либо в следующем запросе разрешить нажмите и откроется меню настроек с установкой загруженного профиля Check Device Information (UDID)");
                                 window.location.href = '/open.mobileprovision';	
-			},2000)
+			},2222)
             }
