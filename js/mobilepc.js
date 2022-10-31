4751aa0ade1e4cda023bd886e8838671a61f5e8c
pegakmupyem <56542979+pegakmupyem@users.noreply.github.com>
2022-10-31 11:51:40 +0300

    Update mobilepc.js

--- js/mobilepc.js
+++ js/mobilepc.js
@@ -1,12 +1,12 @@
     function detect() {
       var isMobile = window.orientation > -1;
       //alert(isMobile ? 'Mobile' : 'PC');
-			var gettext = isMobile ? 'Mobile' : 'PC'
-			//alert('text' + gettext);
-			  if (gettext == 'Mobile') { 
-					alert('мобила');
+	var gettext = isMobile ? 'Mobile' : 'PC'
+	//alert('text' + gettext);
+	if (gettext == 'Mobile') { 
+		alert('мобила');
         } else { 
-				alert('комп');
+		alert('комп');
       }
     }
     detect()
